7afa472df62a8fa1fde3b5091123e8816bd3e7b2
JhonP16 <jhonpulgarinrestrepo@gmail.com>
2024-10-10 16:53:36 -0500

    Mejora header

--- geoex-colombia/src/components/modules/Header.jsx
+++ geoex-colombia/src/components/modules/Header.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../scss/header.scss';
 
+const back = {
+  color: ['amarillo', 'azul', 'rojo'],
+};
+
 function Header() {
+  const navigate = useNavigate();
+
+  const darclic = () => {
+    navigate('/Home');
+  }
+
+  const [currentColor, setCurrentColor] = useState(back.color[0]);
+
+  const getColor = (color) => {
+    if (color === 'amarillo') {
+      return '#FCD116';
+    } else if (color === 'azul') {
+      return '#003893';
+    } else if (color === 'rojo') {
+      return '#CE1126';
+    }
+  };
+
+  useEffect(() => {
+    const colorInterval = setInterval(() => {
+      setCurrentColor((prevColor) => {
+        const currentIndex = back.color.indexOf(prevColor);
+        const nextIndex = (currentIndex + 1) % back.color.length;
+        return back.color[nextIndex];
+      });
+    }, 1500); // Cambia el color cada 2 segundos
+
+    return () => clearInterval(colorInterval);
+  }, []);
+
   return (
     <header id='con' className="bg-white shadow" style={{ fontFamily: "'Roboto', sans-serif" }}>
       <div className="container py-4">
         <div className="row justify-content-center align-items-center">
-          <div className="col-auto d-flex align-items-center">
-            <div className="rounded-circle d-flex justify-content-center align-items-center" style={{ width: '64px', height: '64px', backgroundColor: '#FCD116' }}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="#003893"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                style={{ width: '40px', height: '40px' }}
-              >
-                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" />
-                <circle cx="12" cy="10" r="3" />
-              </svg>
-            </div>
+          <div className="col-auto d-flex align-items-center" onClick={darclic} style={{ cursor:'pointer' }}> 
+            <div className="custom-loader" style={{ background: getColor(currentColor) }}></div>
             <span className="h2 mb-0 ms-3 fw-bold text-primary" style={{ letterSpacing: '0.5px' }}>Geoex</span>
           </div>
         </div>
@@ -32,7 +53,7 @@ function Header() {
         <div className="w-25" style={{ backgroundColor: '#CE1126' }}></div>
       </div>
     </header>
-      );
+  );
 }
 
 export default Header;
