dea413817439d151fbee9103a0a014a4927c7f16
CustomFit-dev <palaciosangeldavidn@gmail.com>
2024-10-10 12:33:46 -0500

    Recreaccion
    Se recreo el proyecto en este nuevo repositorio

---
+++ .history/geoex-colombia/src/components/Home_20241009222836.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './modules/Header';
+
+function Home() {
+  return (
+    <div class="container-fluid">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
---
+++ .history/geoex-colombia/src/components/Home_20241009223456.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './modules/Header';
+import Sec1 from './modules/Sec1';
+
+function Home() {
+  return (
+    <div class="container-fluid">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
